aa38038e3152fc13c820674fb4bfbc11c614e030
bryan <bryanhernandz102@gmail.com>
2022-08-20 21:22:48 -0300

    this is amazing

--- src/container/Technologies/Technologies.jsx
+++ src/container/Technologies/Technologies.jsx
@@ -2,26 +2,30 @@ import React from "react";
 import "./Technologies.scss";
 import { motion } from "framer-motion";
 // import ReactToolTip from "react-tooltip";
-// import { AppWrap } from "../../wrapper";
+import { AppWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 // import { GiSkills } from "react-icons/gi";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Technologies = () => {
-  const [experiences, setExperience] = React.useState([]);
   const [skills, setSkills] = React.useState([]);
   React.useEffect(() => {
-    const query = '*[_type == "experiences"]';
     const skillsQuery = '*[_type == "skills"]';
-    client.fetch(query).then((data) => {
-      setExperience(data);
-    });
     client.fetch(skillsQuery).then((data) => {
       setSkills(data);
     });
   }, []);
   return (
     <section id="technologies">
-      <h2 className="head-text">Skill & Expiences</h2>
+      <h2 className="head-text">Technologies🧰Techtools</h2>
       <div className="app__skill-container">
         <motion.div className="app__skills-list">
           {skills.map((skill) => (
@@ -36,14 +40,37 @@ const Technologies = () => {
                 style={{ backgroundColor: skill.bgColor }}
               >
                 <img src={urlFor(skill.icon)} alt={skill.name} />
-                <p className="p-text">{skill.name}</p>
               </div>
+              <p className="p-text">{skill.name}</p>
             </motion.div>
           ))}
         </motion.div>
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
 };
 
-export { Technologies };
+export default AppWrap(Technologies, "app__skills");
+// export { Technologies };
--- src/container/index.js
+++ src/container/index.js
@@ -1,9 +1,8 @@
 import Header from "./Header/Header";
 import About from "./About/About";
 import Projects from "./Projects/Projects";
-
-export { Technologies } from "./Technologies/Technologies";
+import Technologies from "./Technologies/Technologies";
 export { Footer } from "./Footer/Footer";
 export { Contact } from "./Contact/Contact";
 
-export { Header, About, Projects };
+export { Header, About, Projects, Technologies };
